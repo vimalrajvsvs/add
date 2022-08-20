aa59a0a13217db67062a16dc1b958330dbd299d7
vimalrajvsvs <111408973+vimalrajvsvs@users.noreply.github.com>
2022-08-20 19:28:36 +0530

    Add files via upload

---
+++ addoreven.java
@@ -0,0 +1,22 @@
+
+public class addoreven {
+
+	void add(int a) {
+
+
+		if (a % 2 == 0) {
+			System.out.println("Even number");
+
+		} else {
+			System.out.println("odd nuber");
+
+		}
+	}
+
+	public static void main(String[] args) {
+		addoreven  vimal = new addoreven ();
+		vimal.add(4);
+
+	}
+}
+

4907dabc8cd9e770bc85cd2280e8fcebb6c14ac7
vimalrajvsvs <111408973+vimalrajvsvs@users.noreply.github.com>
2022-08-23 23:27:15 +0530

    Add files via upload

---
+++ findmax.java
@@ -0,0 +1,11 @@
+public class findmax{
+    void add(){
+       int a=578;
+       int b=8765;
+        System.out.println("Result:"+Math.max(a,b));
+      }
+ public static void main(String arg[]){
+    findmax me=new findmax();
+    me.add();
+   }
+}

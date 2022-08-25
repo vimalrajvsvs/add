875a69dbe6386544a90c4746e2a0e0af97330e0b
vimalrajvsvs <111408973+vimalrajvsvs@users.noreply.github.com>
2022-08-25 18:06:53 +0530

    Add files via upload

--- least_comman.java
+++ least_comman.java
@@ -1,21 +1,58 @@
+/*public class least_comman{
+    void main(){
+      int a=4;
+      int b=9;
+      int lcm=0;
+      int max=(a>b)? a:b;
+         while(true)
+          {
+            if(max%a==0&&max%b==0)
+              {
+                lcm=max;
+                break;
+               }
+                max++;
+            }
+                 System.out.println(lcm);
+ }
+public static void main(String arg[]){
+    least_comman me=new least_comman();
+    me.main();
+      }
+}*/
 public class least_comman{
-    void add(){
-     int a=4;
-     int b=10;
-     int gcd=1;
-     for(int i=1;i<=a/2&&i<=b/2;i++)
-        {
-       if(a%i==0&&b%i==0)
-            gcd=i;
-         }
-      int lcm=(a*b)/gcd;
-         {
-
-            System.out.println(lcm);
-         }
+   void add(){
+       int a=5; 
+       int b=6;
+       int c=4;
+       int lcm=0;
+       int max=0;
+         if(a>b&&a>c)
+           { 
+              max=a;
+           }
+       else if(b>c&&b>a)
+           { 
+             max=b;
+            }
+       else
+             {
+               max=c;
+              } 
+       while(true)
+         { 
+            if(max%a==0 &&max%b==0 && max%c==0)
+              { 
+               lcm=max;
+               break;
+              } 
+              max++;
+          } 
+              System.out.println(lcm); 
   }
 public static void main(String arg[]){
-     least_comman me=new least_comman();
-     me.add();
-   }
+    least_comman me=new least_comman();
+    me.add();
+  }
 }
+            
